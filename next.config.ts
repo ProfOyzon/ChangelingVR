0061539c701e84b9d0a7ed2a64cc495bc384169c
xentriom <c04jason@gmail.com>
2025-08-01 09:00:23 -0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   images: {
-    remotePatterns: [
-      new URL(`${process.env.NEXT_PUBLIC_SUPABASE_URL}/**`),
-      new URL('https://xkdlj9yxxa926ujy.public.blob.vercel-storage.com/**'),
-    ],
+    remotePatterns: [new URL('https://xkdlj9yxxa926ujy.public.blob.vercel-storage.com/**')],
   },
 };
 
